8f25a3f584f491a827583ae7282214a883782b19
Santoshb22 <sb289848@gmail.com>
2025-06-22 08:21:33 +0530

    add env to gitignore, create user model and some route structure

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,10 @@
 const express = require("express");
-
+const routes = require("./routes/index");
 const app = express();
 
 app.use(express.urlencoded({extended: true, limit: "16kb"}));
 app.use(express.json({limit: "16kb"}))
 
+app.use("/babysteps/api/v1", routes);
 
 module.exports = app;

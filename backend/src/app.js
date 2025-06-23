eecfc80a9b753f60b806c4263a94bc4524f2c658
Santoshb22 <sb289848@gmail.com>
2025-06-23 18:29:23 +0530

    added cookie-parser and cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const routes = require("./routes/index");
 const app = express();
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+app.use(cors({
+  origin: "http://localhost:5173",  // ✅ your Vite dev server
+  credentials: true                // ✅ allow cookies
+}));
 
 app.use(express.urlencoded({extended: true, limit: "16kb"}));
 app.use(express.json({limit: "16kb"}))
+app.use(cookieParser())
 
 app.use("/api", routes);
 

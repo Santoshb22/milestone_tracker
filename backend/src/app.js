6230b2ea83fa3ba23dece4ac694628fcbc32e7c2
Santosh Bhardwaj <128233053+Santoshb22@users.noreply.github.com>
2025-06-24 10:53:21 +0530

    Update app.js add multiple frontend link

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,10 +4,15 @@ const app = express();
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
+const allowedOrigins = [
+  "https://milestone-tracker-iota.vercel.app",
+  "http://localhost:5173"
+]
+
 app.use(cors({
-  origin: "https://milestone-tracker-iota.vercel.app",  
-  credentials: true        
-}));
+  origin: allowedOrigins,
+  credentials: true
+}))
 
 app.use(express.urlencoded({extended: true, limit: "16kb"}));
 app.use(express.json({limit: "16kb"}))
